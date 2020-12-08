a5540647a763b00e9ed8c69ead9498d799daef1c
Ishan <ishanjaiswal99@gmail.com>
2020-12-08 18:21:13 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,131 @@
+function openOffersDialog() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup').css('display','block');
+        $('#boxpopup').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
+
+
+function openOffersDialog1() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup1').css('display','block');
+        $('#boxpopup1').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog1(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
+
+
+function openOffersDialog2() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup2').css('display','block');
+        $('#boxpopup2').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog2(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
+
+
+function openOffersDialog3() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup3').css('display','block');
+        $('#boxpopup3').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog3(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
+
+
+function openOffersDialog4() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup4').css('display','block');
+        $('#boxpopup4').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog4(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
+
+
+
+function openOffersDialog5() {
+	$('#overlay').fadeIn('fast', function() {
+		$('#boxpopup5').css('display','block');
+        $('#boxpopup5').animate({'left':'30%'},500);
+    });
+}
+
+
+function closeOffersDialog5(prospectElementID) {
+	$(function($) {
+		$(document).ready(function() {
+			$('#' + prospectElementID).css('position','absolute');
+			$('#' + prospectElementID).animate({'left':'-100%'}, 500, function() {
+				$('#' + prospectElementID).css('position','fixed');
+				$('#' + prospectElementID).css('left','100%');
+				$('#overlay').fadeOut('fast');
+			});
+		});
+	});
+}
