f18aa69c95e6a1f1422aa6eda58f75f09bf5e83c
Ishan <ishanjaiswal99@gmail.com>
2020-12-16 20:16:35 +0530

    jsheaderandfooter v1

---
+++ js/header-footer.js
@@ -0,0 +1,73 @@
+const jsheaderTop =  `
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  	<tr>
+    <td width="15%"><img src="images/contact.png" width="32" height="39" alt="login_img" /></td>
+    <td width="55%"><p>+91 8232027205 (Office)</p><p>+91 9051433629 (Mohit)</p></td>
+    <td width="30%" rowspan="2"><img src="images/iso.png" width="79" height="77" alt="" /></td>
+    </tr>
+    <tr>
+    <td width="15%"><img src="images/email.png" width="32" height="42" alt="login_img" /></td>
+    <td width="55%"><p>prind2016@gmail.com</p><p>mohitjaiswal20007@gmail.com</p></td>
+    </tr>
+    </table>`;
+
+const menujs = `
+            <li><a href="pulleys-castings.html">Cast Iron Pulleys Castings</a></li>
+            <li><a href="plummer-block-housing.html">Cast Iron Plummer Block Housing Castings</a></li>
+            <li><a href="cast-iron-valves.html">Cast Iron Valve &amp; Valve Parts Castings</a></li>
+            <li><a href="pump-casting.html">Cast Iron Pump Casting Components</a></li>
+            <li><a href="gear-box-housing.html">Cast Iron Gear Box Housing Casting</a></li>
+            <li><a href="ornamental-casting.html">Ornamental Casting</a></li>
+            <li><a href="water-pipeline.html">Water Pipeline</a></li>
+            <li><a href="shutter-gate.html">Shutter Gate and Parts Casting</a></li>
+            <li><a href="electrical-hightension-line.html">Electrical High-Tension Line</a></li>
+            <li><a href="housing-casting.html">Housing Casting</a></li>
+            <li><a href="cast-iron-weights.html">Cast Iron Weights</a></li>
+            <li><a href="cast-iron-fly-wheels.html">Cast Iron Fly Wheels</a></li>
+            <li><a href="cast-iron-handwheels.html">Cast Iron Handwheels</a></li>
+            <li><a href="cast-iron-gears-castings.html">Cast Iron Gears Castings</a></li>
+            <li><a href="cast-iron-bollards.html">Cast Iron Bollards</a></li>
+            <li><a href="cast-iron-linear-plates.html">Cast Iron Linear Plates</a></li>
+            <li><a href="cast-iron-base-plates.html">Cast Iron Base Plates</a></li>
+            <li><a href="cast-iron-industrial-castings.html">Cast Iron Industrial Castings</a></li>
+            <li><a href="automotive-castings.html">Automotive Castings</a></li>
+            <li><a href="railway-castings.html">Railway Castings as per RDSO</a></li>
+            <li><a href="other-castings.html">Other Castings</a></li>
+            `;
+
+const jsfooter = `<div class="footerTop">
+<div class="footerTopLeft">
+    <h2>P. R. Industries</h2>
+    <p>With a Manufacturing experience of around 30+ years, P. R. Industries has become a 
+    pioneer in the production and supplying of premium grade grey castings.</p>
+</div><!--end of footerTopLeft-->
+<div class="footerTopRight">
+    <div class="footerTopRightInner">
+    <img src="images/contact_icon.jpg" width="20" height="21" alt="" style="float:left; margin:0 10px 0 0;" />
+    <p>Mohit Jaiswal </p>
+    </div><!--end of footerTopRightInner-->
+    <div class="footerTopRightInner">
+    <img src="images/mobile_bg.jpg" width="20" height="21" alt="" style="float:left; margin:0 10px 0 0;" />
+    <p> +91 9051433629</p>
+    </div><!--end of footerTopRightInner-->
+    <div class="footerTopRightInner">
+    <img src="images/email_icon.jpg" width="20" height="21" alt="" style="float:left; margin:0 10px 0 0;" />
+    <p>mohitjaiswal20007@gmail.com</p>
+    </div><!--end of footerTopRightInner-->
+</div><!--end of footerTopRight-->
+</div><!--end of footerTop-->
+
+<div class="footerBottom">
+<div class="footerBottomInner">
+<!-- <div class="footerBottomInnerLeft"><p><a href="http://techonindia.com/"  target="_blank">Designed by "TechonIndia Infoservices"</a></p></div> -->
+<div class="footerBottomInnerRight">
+<p>
+<a href="about-us.html">About</a> | 
+<a href="desigen-development.html">Design &amp; Development</a> | 
+<a href="foundry-facilities.html">Foundry Facilities</a> | 
+<a href="quality.html">Quality</a> | 
+<a href="contact-us.html">Contact Us</a>
+</p></div>
+</div><!--end of footerBottomInner-->
+</div><!--end of footerBottom-->
+`;
